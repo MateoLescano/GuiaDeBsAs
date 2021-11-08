366789381bb6b19284011a7539421702d17cfd02
MateoLescano <mateolescano@gmail.com>
2021-11-08 17:26:05 -0300

    location

--- app/src/main/java/com/citesoftware/guiadebsas/ui/home/HomeFragment.kt
+++ app/src/main/java/com/citesoftware/guiadebsas/ui/home/HomeFragment.kt
@@ -1,40 +1,78 @@
 package com.citesoftware.guiadebsas.ui.home
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.citesoftware.guiadebsas.MainActivity
 import com.citesoftware.guiadebsas.R
 import com.citesoftware.guiadebsas.databinding.FragmentHomeBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 class HomeFragment : Fragment() {
 
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private lateinit var lastLocation: Location
+
+
+    @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
+
+        setUpMap()
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        googleMap.uiSettings.isMyLocationButtonEnabled = true
+        googleMap.uiSettings.isRotateGesturesEnabled = true
+        googleMap.uiSettings.isCompassEnabled = true
+
+
+        googleMap.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(requireActivity()) { location ->
+
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 14f))
+            }
+        }
+
+
         val loc1 = LatLng(-34.575801, -58.463913)
         val loc2 = LatLng( -34.5781574,  -58.4608209)
 
+
+
         googleMap.addMarker(MarkerOptions().position(loc1).title("Casa"))
         googleMap.addMarker(MarkerOptions().position(loc2).title("Pizza"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc1,15.0f))
+
+
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc1,15.0f))
+
+
+
     }
 
     override fun onCreateView(
@@ -42,6 +80,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
@@ -50,4 +89,16 @@ class HomeFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+    }
+
+
+
 }
