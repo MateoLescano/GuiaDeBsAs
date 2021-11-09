3d82e6c44bf08039efd19dd9dd29e03907cb1d06
MateoLescano <mateolescano@gmail.com>
2021-11-09 19:11:23 -0300

    RecyclerView Lista

--- app/src/main/java/com/citesoftware/guiadebsas/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/citesoftware/guiadebsas/ui/gallery/GalleryFragment.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.citesoftware.guiadebsas.R
 import com.citesoftware.guiadebsas.databinding.FragmentGalleryBinding
 
@@ -16,9 +18,16 @@ class GalleryFragment : Fragment() {
     private lateinit var galleryViewModel: GalleryViewModel
     private var _binding: FragmentGalleryBinding? = null
 
+    var titulos = arrayOf("La Mezzetta", "Mi casa", "El clu")
+    var tipo = arrayOf("Resto", "Sitio Historico", "Trimarchi")
+    var direccion = arrayOf("Av. 123", "Av. 232332", "Calle 2323")
+    var img = arrayOf(R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_foreground)
+
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +44,13 @@ class GalleryFragment : Fragment() {
 //        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
+
+        val adapter = RVListaAdapter(titulos, tipo, direccion, img)
+        val recyclerView: RecyclerView = root.findViewById(R.id.rvLista)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
         return root
     }
 
--- app/src/main/java/com/citesoftware/guiadebsas/ui/gallery/RVListaAdapter.kt
+++ app/src/main/java/com/citesoftware/guiadebsas/ui/gallery/RVListaAdapter.kt
@@ -1,4 +1,45 @@
 package com.citesoftware.guiadebsas.ui.gallery
 
-class RVListaAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.citesoftware.guiadebsas.R
+
+class RVListaAdapter(private var TitulosLoc: Array<String>, private var TipoLoc: Array<String>, private var DireccionLoc: Array<String>, private var ImgLoc: Array<Int>): RecyclerView.Adapter<RVListaAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+        var itemImage: ImageView = itemView.findViewById(R.id.imgItem)
+
+        var titulo: TextView = itemView.findViewById(R.id.tvTitulo)
+
+        var tipo: TextView = itemView.findViewById(R.id.tvTipo)
+
+        var direccion: TextView = itemView.findViewById(R.id.tvDireccion)
+
+//        init {
+//
+//        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_lista, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.titulo.text = TitulosLoc[position]
+        holder.tipo.text = TipoLoc[position]
+        holder.direccion.text = DireccionLoc[position]
+        holder.itemImage.setImageResource(ImgLoc[position])
+
+    }
+
+    override fun getItemCount(): Int = TitulosLoc.size
 }
